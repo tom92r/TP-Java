bfad3b3ed4202b6d721f0b130ea859db33f1595f
tom92r <thomas.gibert@student.junia.com>
2026-02-02 10:38:08 +0100

    Initial commit - GenreDao finished

---
+++ src/main/java/fr/isen/java2/db/daos/MovieDao.java
@@ -0,0 +1,20 @@
+package fr.isen.java2.db.daos;
+
+import java.util.List;
+
+import fr.isen.java2.db.entities.Movie;
+
+public class MovieDao {
+
+	public List<Movie> listMovies() {
+		throw new RuntimeException("Method is not yet implemented");
+	}
+
+	public List<Movie> listMoviesByGenre(String genreName) {
+		throw new RuntimeException("Method is not yet implemented");
+	}
+
+	public Movie addMovie(Movie movie) {
+		throw new RuntimeException("Method is not yet implemented");
+	}
+}
---
+++ src/test/java/fr/isen/java2/db/daos/MovieDaoTestCase.java
@@ -0,0 +1,54 @@
+package fr.isen.java2.db.daos;
+
+import static org.assertj.core.api.Assertions.fail;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MovieDaoTestCase {
+	@BeforeEach
+	public void initDb() throws Exception {
+		Connection connection = DataSourceFactory.getDataSource().getConnection();
+		Statement stmt = connection.createStatement();
+		stmt.executeUpdate(
+				"CREATE TABLE IF NOT EXISTS genre (idgenre INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT , name VARCHAR(50) NOT NULL);");
+		stmt.executeUpdate(
+				"CREATE TABLE IF NOT EXISTS movie (\r\n"
+				+ "  idmovie INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\r\n" + "  title VARCHAR(100) NOT NULL,\r\n"
+				+ "  release_date DATETIME NULL,\r\n" + "  genre_id INT NOT NULL,\r\n" + "  duration INT NULL,\r\n"
+				+ "  director VARCHAR(100) NOT NULL,\r\n" + "  summary MEDIUMTEXT NULL,\r\n"
+				+ "  CONSTRAINT genre_fk FOREIGN KEY (genre_id) REFERENCES genre (idgenre));");
+		stmt.executeUpdate("DELETE FROM movie");
+		stmt.executeUpdate("DELETE FROM genre");
+		stmt.executeUpdate("DELETE FROM sqlite_sequence WHERE name='movie'");
+		stmt.executeUpdate("DELETE FROM sqlite_sequence WHERE name='genre'");
+		stmt.executeUpdate("INSERT INTO genre(idgenre,name) VALUES (1,'Drama')");
+		stmt.executeUpdate("INSERT INTO genre(idgenre,name) VALUES (2,'Comedy')");
+		stmt.executeUpdate("INSERT INTO movie(idmovie,title, release_date, genre_id, duration, director, summary) "
+				+ "VALUES (1, 'Title 1', '2015-11-26 12:00:00.000', 1, 120, 'director 1', 'summary of the first movie')");
+		stmt.executeUpdate("INSERT INTO movie(idmovie,title, release_date, genre_id, duration, director, summary) "
+				+ "VALUES (2, 'My Title 2', '2015-11-14 12:00:00.000', 2, 114, 'director 2', 'summary of the second movie')");
+		stmt.executeUpdate("INSERT INTO movie(idmovie,title, release_date, genre_id, duration, director, summary) "
+				+ "VALUES (3, 'Third title', '2015-12-12 12:00:00.000', 2, 176, 'director 3', 'summary of the third movie')");
+		stmt.close();
+		connection.close();
+	}
+	
+	 @Test
+	 public void shouldListMovies() {
+		 fail("Not yet implemented");
+	 }
+	
+	 @Test
+	 public void shouldListMoviesByGenre() {
+		 fail("Not yet implemented");
+	 }
+	
+	 @Test
+	 public void shouldAddMovie() throws Exception {
+		 fail("Not yet implemented");
+	 }
+}
